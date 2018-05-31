27d69d90e532210cf6555c0272d224e98debcf43
maryann-achi <32832248+maryann-achi@users.noreply.github.com>
2018-05-31 16:00:18 +0400

    Testing change

--- AttendanceAppMaryAchieng/src/controllers/AttendanceController_Increment1.java
+++ AttendanceAppMaryAchieng/src/controllers/AttendanceController_Increment1.java
@@ -17,91 +17,91 @@ import repositories.Repository;
 public class AttendanceController_Increment1 {
      private final Repository repository;
      List<Swipe>swipes;
-    
+
     /**
      *
      */
-        
+
     public AttendanceController_Increment1() {
-       
+
         InputHelper inputHelper = new InputHelper();
         char a = inputHelper.readCharacter("Load an already existing Customers File (Y/N)?");
         if (a == 'Y' || a == 'y') {
-            String fileName = inputHelper.readString("Enter filename");               
+            String fileName = inputHelper.readString("Enter filename");
             this.repository = new Repository(fileName);
         }
         else {
             this.repository = new Repository();
         }
     }
-   
+
     /**
      *
      */
     public void run() {
         boolean finished = false;
-        
+
         do {
             char choice = displayAttendanceMenu();
             switch (choice) {
-                case 'A': 
+                case 'A':
                     addSwipe();
                     break;
-                case 'B':  
+                case 'B':
                     listSwipes();
                     break;
-                case 'C': 
-                    listSwipesByCardIdOrderedByDateTime(); // 
-                    break;                    
-                case 'D': 
+                case 'C':
+                    listSwipesByCardIdOrderedByDateTime(); //
+                    break;
+                case 'D':
                     listSwipeStatistics(); //
                     break;
-                case 'Q': 
+                case 'Q':
                     finished = true;
             }
         } while (!finished);
     }
-    
+
     private char displayAttendanceMenu() {
         InputHelper inputHelper = new InputHelper();
         System.out.print("\nA. Add Swipe");
-        System.out.print("\tB. List Swipes");        
+        System.out.print("\tB. List Swipes");
         System.out.print("\tC. List Swipes In Date Time Order");
-        System.out.print("\tD. List Swipes Which Match Card Id"); 
-        System.out.print("\tQ. Quit\n");         
+        System.out.print("\tD. List Swipes Which Match Card Id");
+        System.out.print("\tQ. Quit\n");
         return inputHelper.readCharacter("Enter choice", "ABCDQ");
-    }    
-    
+    }
+
     private void addSwipe() {
         System.out.format("\033[31m%s\033[0m%n", "Add Swipe");
         System.out.format("\033[31m%s\033[0m%n", "=========");
-        
-    
+
+
     }
-    
-    private void listSwipes() {        
+
+    private void listSwipes() {
         System.out.format("\033[31m%s\033[0m%n", "Swipes");
         System.out.format("\033[31m%s\033[0m%n", "======");
-       
+
         Iterator it = repository.getItems().listIterator();
         while (it.hasNext()) {
             Swipe s = (Swipe) it.next();
             System.out.println(s);
-        }        
-    }      
-      
+        }
+    }
 
-    private void listSwipesByCardIdOrderedByDateTime() {  
+
+    private void listSwipesByCardIdOrderedByDateTime() {
         System.out.format("\033[31m%s\033[0m%n", "Swipes By Card Id");
         System.out.format("\033[31m%s\033[0m%n", "=================");
-        
-        }        
-   
+
+        }
+
 
     private void listSwipeStatistics() {
         System.out.format("\033[31m%s\033[0m%n", "Swipe Statistics for room");
-        System.out.format("\033[31m%s\033[0m%n", "========================="); 
-        
-        
+        System.out.format("\033[31m%s\033[0m%n", "=========================");
+
+
     }
 }
